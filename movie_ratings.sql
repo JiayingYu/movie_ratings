a5f50d2e3d5ef399a3d7a683860a16f034fd6ae2
Jiaying Yu <jiaying.yu@nyu.edu>
2014-03-31 18:38:36 -0400

    first commit

---
+++ movie_ratings.sql
@@ -0,0 +1,65 @@
+/*load table movie_ratings*/
+DROP TABLE IF EXISTS movie_ratings;
+CREATE TABLE movie_ratings (
+	user_id varchar(3),
+	item_id varchar(4),
+	rating int(1),
+	time_stamp varchar(20),
+	PRIMARY KEY (time_stamp),
+	FOREIGN KEY (user_id) REFERENCES user (id),
+	FOREIGN KEY (item_id) REFERENCES movie_items (id)
+	);
+
+LOAD DATA LOCAL INFILE 'u.data'
+	INTO TABLE movie_ratings;
+
+/*load table movie_items*/
+DROP TABLE IF EXISTS movie_items;
+CREATE TABLE movie_items (
+	id varchar(4),
+	title varchar(255),
+	release_date char(11),
+	video_release_date char(11),
+	imdb_url text,
+	unknown int(1),
+	action int(1),
+	adventure int(1),
+	animation int(1),
+	childerns int(1),
+	comedy int(1),
+	crime int(1),
+	documentary int(1),
+	drama int(1),
+	fantasy int(1),
+	film_noir int(1),
+	horror int(1),
+	musical int(1),
+	mystey int(1),
+	romance int(1),
+	sci_fi int(1),
+	thriller int(1),
+	war int(1),
+	western int(1),
+	PRIMARY KEY (id)
+	);
+
+LOAD DATA LOCAL INFILE 'u.item'
+INTO TABLE movie_items
+FIELDS TERMINATED BY '|'
+LINES TERMINATED BY '\n';
+ 
+/*load table user*/ 
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
+	id varchar(3),
+	age int(2),
+	gender char(1),
+	occupation varchar(20),
+	zip_code char(5),
+	PRIMARY KEY (id)
+);
+
+LOAD DATA LOCAL INFILE 'u.user'
+INTO TABLE user
+FIELDS TERMINATED BY '|'
+LINES TERMINATED BY '\n';
